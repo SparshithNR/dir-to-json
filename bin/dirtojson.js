e67dd921f407ccea3b77f8af7139c85f397d17cb
sparshithNR <nairbali@usc.edu>
2019-10-11 10:36:06 -0700

    Adding command line interface and CI
    1. Added travis CI
    2. appveyor CI
    3. Provide command line interface

---
+++ bin/dirtojson.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+const [,, ...args] = process.argv;
+
+const dirtojson = require('../index');
+const chalk = require('chalk');
+if (args.length > 1) {
+  console.log(chalk.red(`Invalid usage. Please use dirtojson [path]`));
+  return;
+}
+
+if (args.length == 0) {
+  console.log(chalk.green(JSON.stringify(dirtojson('./'), null, 2)));
+  return;
+}
+
+return console.log(chalk.green(JSON.stringify(dirtojson(args[0]), null, 2)));
